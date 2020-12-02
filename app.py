9a3107c6c1e6001eb2e613ddf09c2e78e2a83359
Mattiejordan <jordan.mattiem@gmail.com>
2020-12-02 04:19:20 -0600

    9.5.1 is confusing

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
+#9.5.1
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
 #9.4.3
+
 pip install flask
 from flask import Flask
 
@@ -11,3 +25,39 @@ def hello_world():
 export FLASK_APP=app.py
 set FLASK_APP=app.py
 flask run 
+
+#9.5.1 Set up the database
+#not sure if it goes in here or a jupyter notebook???
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+#set up flask
+app = Flask(__name__)
+import app
+
+print("example __name__ = %s", __name__)
+
+if __name__ == "__main__":
+    print("example is being run directly.")
+else:
+    print("example is being imported")
+
+# set up routes AFTER the app=Flask(__name__)
+#9.5.2
+@app.route("/")
+
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
