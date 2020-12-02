2b1bd399341cf6b7c08b811a47f4148ff14e65e8
Mattiejordan <jordan.mattiem@gmail.com>
2020-12-02 03:39:44 -0600

    9.4.3 python error install flask

---
+++ app.py
@@ -0,0 +1,13 @@
+#9.4.3
+pip install flask
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world'
+
+export FLASK_APP=app.py
+set FLASK_APP=app.py
+flask run 
