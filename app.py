c7fa503c4bf0ec606e34dd1cf1f20e968992bccb
Mattiejordan <jordan.mattiem@gmail.com>
2020-12-03 20:12:00 -0600

    Finally fixed sqlite hawaii!

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ from flask import Flask, jsonify
 
 #9.4.3
 
-pip install flask
+#pip install flask
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+#@app.route('/')
 def hello_world():
     return 'Hello world'
 
@@ -61,3 +61,20 @@ def welcome():
     /api/v1.0/tobs
     /api/v1.0/temp/start/end
     ''')
+
+#9.5.3
+@app.route("/api/v1.0/precipitation")
+
+# def precipitation():
+#    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+#    precipitation = session.query(Measurement.date, Measurement.prcp).\
+#       filter(Measurement.date >= prev_year).all()
+#    return
+# JASONIFY IT
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
